83964fd09ce0cd9147a2e4d272dcd3cc8e133210
Robertslob <rjrslob@gmail.com>
2016-02-24 17:57:51 +0100

    sb is er, nog niet bij route

--- Program.cs
+++ Program.cs
@@ -20,20 +20,20 @@ namespace Algoritmiek2
             s = Console.ReadLine();
             strings = s.Split(' '); // De tekst
             opti opt = new opti(modus, L, strings);
+            //Console.SetIn(new StreamReader(Console.OpenStandardInput()));
             opt.calculate();
             if (modus == 1)
             {
                 ushort word = 0;
-                //string copy = "";
                 StringBuilder sb = new StringBuilder();
                 string[] route = opt.finalRoute.Split(',');
                 for (ushort counter = 0; counter < route.Length; counter++)
-                {
+                {                    
                     ushort enter = ushort.Parse(route[counter]);
                     for (; word < enter; word++)
                     {
-                        //copy += strings[word];                        
                         sb.Append(strings[word] + " ");
+                        
                     }
                     sb.Append(strings[word]);
                     word++;
@@ -43,7 +43,6 @@ namespace Algoritmiek2
                 Console.WriteLine(sb.ToString());
             }
             else Console.WriteLine(opt.finalScore);
-            
             Console.ReadLine();
         }
     }
@@ -81,15 +80,13 @@ namespace Algoritmiek2
             {
                 if (n < lastIndex)
                 {
-                    /*if (scores[n, regelLengte] != 0)
-                        return;*/
                     string woord = strings[n];
                     short nieuweLengte = (short)(regelLengte - woord.Length - 1); // -1 voor spatie
                     ushort firstScore;
                     char c = woord[woord.Length - 1];
                     ushort isLeesteken = (ushort)((c == '?' | c == '.' | c == '!') ? 0 : 1);
                     ushort enterScore = (ushort)(scores[currentIndex,L] + isLeesteken * (nieuweLengte + 1) * (nieuweLengte + 1)); // enter erbij
-                    if (nieuweLengte >= strings[n + 1].Length) // 1 spatie te veel, dus plaats voor 1 letter, groter dan 0 denk ik (>=0 safe)
+                    if (nieuweLengte >= strings[n + 1].Length) 
                     {
                         firstScore = scores[currentIndex, nieuweLengte];
                         if (firstScore < enterScore)
@@ -178,214 +175,6 @@ namespace Algoritmiek2
             finalScore = scores[currentIndex, L];
             if(modus == 1)
                 finalRoute = routes[currentIndex, L];
-        }
-
-        /*ushort opt(ushort i, ushort j)
-        {
-            if (i == j)
-                return Functie(strings[i]);
-            ushort sum = 0;
-            for (ushort k = i; k <= j; k++)
-            {
-                sum += (ushort)(strings[k].Length +1); //+1 voor de spatie
-                if(sum -1 > L)
-                    break;
-            }
-            sum -= 1;
-            if(sum>L){
-                ushort min = (ushort)(opt(i, i) + opt((ushort)(i+1), j));
-                for(ushort r = (ushort)(i+1); r<j; r++)
-                {
-                    ushort score = (ushort)(opt(i, r) + opt((ushort)(r+1), j));
-                    if(score < min){
-                        min = score;
-                    }
-                }
-                return min;
-            }
-            else{
-                // Denk dat het zonder onderstaande al werkt, maar not sure
-                 ushort min = (ushort)(opt(i, i) + opt((ushort)(i+1), j));
-                for(ushort r = (ushort)(i+1); r<j; r++)
-                {
-                    ushort score = (ushort)(opt(i, r) + opt((ushort)(r+1), j));
-                    if(score < min){
-                        min = score;
-                    }
-                }
-                return min; 
-                char c = strings[j].Last();
-                ushort isLeesteken = (ushort)((c == '?' | c == '.' | c == '!') ? 0 : 1);
-                return (ushort)((L - sum) * isLeesteken);
-            }
-        }*/
-
-        /*public void calculator()
-        {
-            ushort[,] scores = new ushort[lastIndex + 1, lastIndex + 1];
-            if (modus == 1)
-            {
-                string[,] routes = new string[lastIndex + 1, lastIndex + 1];
-                for (ushort r = 0; r <= lastIndex; r++)
-                {
-                    for (ushort s = 0; s + r <= lastIndex; s++)
-                    {
-                        if (0 == r)
-                        {
-                            scores[s, s + r] = Functie(strings[s]);
-                            routes[s, s + r] = s.ToString() + ",";
-                        }
-
-                        else
-                        {
-                            ushort sum = 0;
-                            for (ushort k = s; k <= s + r; k++)
-                            {
-                                sum += (ushort)(strings[k].Length + 1); //+1 voor de spatie
-                                if (sum - 1 > L)
-                                    break;
-                            }
-                            sum -= 1;
-                            if (sum > L)
-                            {
-                                ushort min = (ushort)(scores[s, s] + scores[s + 1, s + r]);
-                                ushort counter = s;
-                                for (ushort i = (ushort)(s + 1); i < s + r; i++)
-                                {
-                                    ushort score = (ushort)(scores[s, i] + scores[i + 1, s + r]);
-                                    if (score < min)
-                                    {
-                                        min = score;
-                                        counter = i;
-                                    }
-                                }
-                                scores[s, s + r] = min;
-                                routes[s, s + r] = routes[s, counter] + routes[counter + 1, s + r];// counter.ToString() + "," + routes[counter + 1, s + r];
-                            }
-                            else
-                            {
-                                /* Denk dat het zonder onderstaande al werkt, maar not sure
-                                 * 
-                                 * ushort min = (ushort)(opt(i, i) + opt((ushort)(i+1), j));
-                                for(ushort r = (ushort)(i+1); r<j; r++)
-                                {
-                                    ushort score = (ushort)(opt(i, r) + opt((ushort)(r+1), j));
-                                    if(score < min){
-                                        min = score;
-                                    }
-                                }
-                                return min; 
-                                char c = (strings[r + s])[strings[r + s].Length - 1];
-                                ushort isLeesteken = (ushort)((c == '?' | c == '.' | c == '!') ? 0 : 1);
-                                ushort tempScore = (ushort)(L - sum);
-                                ushort min = (ushort)((tempScore * tempScore) * isLeesteken);
-                                //ushort min = (ushort)(scores[s, s] + scores[s + 1, s + r]);
-                                ushort counter = (ushort)(s+r+1);
-                                for (ushort i = s; i < s + r; i++)
-                                {
-                                    ushort score = (ushort)(scores[s, i] + scores[i + 1, s + r]);
-                                    if (score < min)
-                                    {
-                                        min = score;
-                                        counter = i;
-                                    }
-                                }
-                                scores[s, s + r] = min;
-                                /*if (counter != s + r + 1)
-                                    routes[s, s + r] = routes[s, counter] + routes[counter + 1, s + r];
-                                else routes[s, s + r] = (s + r).ToString() + ",";
-
-
-                                /*char c = strings[r + s].Last();
-                                ushort isLeesteken = (ushort)((c == '?' | c == '.' | c == '!') ? 0 : 1);
-                                scores[s, s + r] = (ushort)((L - sum) * isLeesteken);
-                                routes[s, s + r] = (s + r).ToString() + ",";
-                            }
-                        }
-
-                    }
-                }
-                finalScore = scores[0, lastIndex];
-                finalRoute = routes[0, lastIndex];
-            }
-            else
-            {
-                for (ushort r = 0; r <= lastIndex; r++)
-                {
-                    for (ushort s = 0; s + r <= lastIndex; s++)
-                    {
-                        if (0 == r)
-                        {
-                            scores[s, s + r] = Functie(strings[s]);
-                        }
-
-                        else
-                        {
-                            ushort sum = 0;
-                            for (ushort k = s; k <= s + r; k++)
-                            {
-                                sum += (ushort)(strings[k].Length + 1); //+1 voor de spatie
-                                if (sum - 1 > L)
-                                    break;
-                            }
-                            sum -= 1;
-                            if (sum > L)
-                            {
-                                ushort min = (ushort)(scores[s, s] + scores[s + 1, s + r]);
-                                for (ushort i = (ushort)(s + 1); i < s + r; i++)
-                                {
-                                    ushort score = (ushort)(scores[s, i] + scores[i + 1, s + r]);
-                                    if (score < min)
-                                    {
-                                        min = score;
-                                    }
-                                }
-                                scores[s, s + r] = min;
-                            }
-                            else
-                            {
-                                /* Denk dat het zonder onderstaande al werkt, maar not sure
-                                    * 
-                                    * ushort min = (ushort)(opt(i, i) + opt((ushort)(i+1), j));
-                                for(ushort r = (ushort)(i+1); r<j; r++)
-                                {
-                                    ushort score = (ushort)(opt(i, r) + opt((ushort)(r+1), j));
-                                    if(score < min){
-                                        min = score;
-                                    }
-                                }
-                                return min; 
-                                char c = (strings[r + s])[strings[r + s].Length - 1];
-                                ushort isLeesteken = (ushort)((c == '?' | c == '.' | c == '!') ? 0 : 1);
-                                ushort tempScore = (ushort)(L - sum);
-                                ushort min = (ushort)((tempScore * tempScore) * isLeesteken);
-                                //ushort min = (ushort)(scores[s, s] + scores[s + 1, s + r]);
-                                //ushort counter = (ushort)(s + r + 1);
-                                /*for (ushort i = s; i < s + r; i++)
-                                {
-                                    ushort score = (ushort)(scores[s, i] + scores[i + 1, s + r]);
-                                    if (score < min)
-                                    {
-                                        min = score;
-                                        //counter = i;
-                                    }
-                                }
-                                scores[s, s + r] = min;
-                            }
-                        }
-
-                    }
-                }
-                finalScore = scores[0, lastIndex];                
-            }
-        }
-
-        ushort Functie(string k)
-        {
-            char c = k[k.Length - 1];
-            ushort isLeesteken = (ushort)((c == '?' | c == '.' | c == '!') ? 0 : 1);
-            ushort score = (ushort)(L - k.Length);
-            return (ushort)((score * score) * isLeesteken);
-        }*/
+        }        
     }
 }
